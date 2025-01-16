22724f581113dbbf4f359361880a153d619632c7
WardPearce <wardpearce@pm.me>
2025-01-16 13:42:06 +1300

    Started python backend

--- inviterr/src/routes/+layout.server.ts
+++
@@ -1,6 +0,0 @@
-import { serializeMongo } from '$lib/serialize';
-
-export async function load({ locals }) {
-  const customization = await locals.mongo.collection('customization').findOne();
-  return { customization: serializeMongo(customization) };
-}
